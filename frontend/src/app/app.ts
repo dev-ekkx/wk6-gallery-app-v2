b54e8461050ac86b779140acdc9a136ac8370b45
Emmanuel Kpendo <devekkx@gmail.com>
2025-07-08 10:08:15 +0000

    feature: design ui for uploading images to s3

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,67 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'frontend';
+
+  protected images = signal<{ file: File, url: string }[]>([]);
+
+protected onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.processFiles(input.files);
+    }
+  }
+
+  protected onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.currentTarget as HTMLElement;
+    dropZone.classList.add('dragover');
+  }
+
+ protected onDragLeave(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.currentTarget as HTMLElement;
+    dropZone.classList.remove('dragover');
+  }
+
+ protected onDrop(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.currentTarget as HTMLElement;
+    dropZone.classList.remove('dragover');
+
+    if (event.dataTransfer?.files && event.dataTransfer.files.length > 0) {
+      this.processFiles(event.dataTransfer.files);
+    }
+  }
+
+  protected removeImage(index: number): void {
+    console.log('Removing image:', this.images());
+    this.images.set(this.images().splice(index, 1));
+  }
+
+  private processFiles(files: FileList): void {
+    Array.from(files).forEach(file => {
+      if (file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.onload = (e: ProgressEvent<FileReader>) => {
+          if (e.target?.result) {
+            const image = {
+              file,
+              url: e.target.result as string
+            }
+            this.images.set([...this.images(), image])
+          }
+        };
+reader.readAsDataURL(file);
+
+      }
+    });
+  }
+
+  protected uploadToS3(): void {
+  console.log('Uploading files', this.images());
+  }
+
 }
